35de5bdbda1995f7361fc552a50013aa8945522a
sabhishekpratap5 <sabhishekpratap5@gmail.com>
2018-12-27 15:53:11 +0530

    basic

--- tuple/program10.py
+++ tuple/program10.py
@@ -1,5 +1,19 @@
-# Write a Python program to reverse a tuple.
+# # Write a Python program to reverse a tuple.
+#
+# tuple1 = "abhishekSingh"
+# rev_tuple = reversed(tuple1)
+# print(tuple(rev_tuple))
 
-tuple1 = "abhishekSingh"
-rev_tuple = reversed(tuple1)
-print(tuple(rev_tuple))
+
+
+import time
+
+st = time.time()
+a =  tuple(range(0, 100000000))
+
+a = set(a)
+a = list(a)
+print(a)
+ed = time.time()
+to = ed - st
+print(time.time() - st)
